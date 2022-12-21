73e27fbbe431ce433bb241e9be2cda5612fce4c0
forellko <dragonforce97q@gmail.com>
2022-12-21 10:30:27 +0300

    add input with state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
 
   return (
     <div className="App">
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <h1>{count}</h1>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <hr />
+      <input value={text} onChange={(event) => setText(event.target.value)} />
     </div>
   );
 }
