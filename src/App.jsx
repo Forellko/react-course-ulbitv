740a2a9de175580712ecc032229f86e6425d5011
forellko <dragonforce97q@gmail.com>
2023-04-01 12:09:28 +0300

    Add Routes and Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Posts from './pages/Posts';
 
 function App() {
   return (
     <BrowserRouter>
+      <div className="navbar">
+        <div className="navbar__links">
+          <Link to="/about">О сайте</Link>
+          <Link to="/posts">Посты</Link>
+        </div>
+      </div>
       <Routes>
-        <Route path="/" element={<Posts />}></Route>
-        <Route path="/about" element={<About />}></Route>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
