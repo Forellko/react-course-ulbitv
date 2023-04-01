51ba44859adf71872b1ec91c187d26bb2263d0e1
forellko <dragonforce97q@gmail.com>
2023-04-01 16:01:23 +0300

    Move Navigate to array

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,24 +3,25 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from '../router';
 
 export default function AppRouter() {
+  const isAuth = false;
   return (
     <div>
       <Routes>
-        {privateRoutes.map((route) => (
-          <Route
-            path={route.path}
-            element={route.component}
-            exact={route.exact}
-          />
-        ))}
-        {publicRoutes.map((route) => (
-          <Route
-            path={route.path}
-            element={route.component}
-            exact={route.exact}
-          />
-        ))}
-        <Route path="/*" element={<Navigate to="/posts" replace />} />
+        {isAuth
+          ? privateRoutes.map((route) => (
+              <Route
+                path={route.path}
+                element={route.component}
+                exact={route.exact}
+              />
+            ))
+          : publicRoutes.map((route) => (
+              <Route
+                path={route.path}
+                element={route.component}
+                exact={route.exact}
+              />
+            ))}
       </Routes>
     </div>
   );
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import { Navigate } from 'react-router-dom';
 import About from '../pages/About';
 import Login from '../pages/Login';
 import PostIdPage from '../pages/PostIdPage';
@@ -7,8 +8,10 @@ export const privateRoutes = [
   { path: '/about', component: <About />, exact: true },
   { path: '/posts', component: <Posts />, exact: true },
   { path: '/posts/:id', component: <PostIdPage />, exact: true },
+  { path: '/*', component: <Navigate to="/posts" replace />, exact: true },
 ];
 
 export const publicRoutes = [
   { path: '/login', component: <Login />, exact: true },
+  { path: '/*', component: <Navigate to="/login" replace />, exact: true },
 ];
